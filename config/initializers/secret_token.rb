751e077855554024bde84c3b33102463ea638a81
karstenpackmohr <karsten.packmohr@googlemail.com>
2013-04-19 11:58:21 +0200

    Initial commit - Ruby on rails simple mvc app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleMVCApp::Application.config.secret_token = 'a30115365e39283b98b31bfc822a5c6bfec5c1c50625fb1d513d675ee61016b2676475b6b0c22396529cd7d78a2d068921bb48508a72dbeb19e6e57f2e993885'
